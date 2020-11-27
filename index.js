768c5e1a06ea0b54d33e001ccd365a43e296c97b
Toni Dietze <Flupp@users.noreply.github.com>
2020-11-27 14:14:09 +0100

    add red line at 200 cases / 7 d / 100000

--- index.js
+++ index.js
@@ -224,6 +224,15 @@ window.onload = function() {
                 , pointHoverRadius: 0
                 , pointRadius: 0
                 , data: [ { x: 0, y: 50 }, { x: xMax, y: 50 } ]
+                , type: 'line' }
+              , { backgroundColor: colorsNew.dark
+                , borderColor: colorsNew.dark
+                , borderWidth: 1
+                , fill: false
+                , label: '200'
+                , pointHoverRadius: 0
+                , pointRadius: 0
+                , data: [ { x: 0, y: 200 }, { x: xMax, y: 200 } ]
                 , type: 'line' } ] }
           , options:
             { scales: scales
